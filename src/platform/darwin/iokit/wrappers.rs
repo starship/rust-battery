6576c8ea7119b961dc4bac58a4ab3e6e8881b3a4
Xiao Liang <izzyliang@gmail.com>
2026-03-21 14:10:42 -0700

    Migrate to objc2

--- src/platform/darwin/iokit/wrappers.rs
+++ src/platform/darwin/iokit/wrappers.rs
@@ -1,13 +1,23 @@
-use std::mem::{self, MaybeUninit};
+#![allow(deprecated)]
+use std::ffi::CStr;
+use std::mem;
 use std::ops::{Deref, DerefMut};
+use std::ptr::NonNull;
 
-use core_foundation::base::{kCFAllocatorDefault, kCFNull, mach_port_t, CFType, TCFType};
-use core_foundation::dictionary::{CFDictionary, CFMutableDictionary, CFMutableDictionaryRef};
-use core_foundation::string::CFString;
+use mach::port::mach_port_t;
 use mach::{kern_return, mach_port, port, traps};
+use objc2_core_foundation::{
+    kCFAllocatorDefault, CFDictionary, CFMutableDictionary, CFRetained, CFString, CFType,
+};
+use objc2_io_kit::{
+    io_iterator_t, io_object_t, kIOMasterPortDefault, IOIteratorNext, IOMasterPort,
+    IOObjectRelease, IORegistryEntryCreateCFProperties, IOServiceGetMatchingServices,
+    IOServiceMatching,
+};
 
-use super::sys;
-use crate::Result;
+use crate::{Error, Result};
+
+pub const IOPM_SERVICE_NAME: &CStr = c"IOPMPowerSource";
 
 #[derive(Debug)]
 pub struct IoMasterPort(mach_port_t);
@@ -17,29 +27,26 @@ impl IoMasterPort {
         let mut master_port: port::mach_port_t = port::MACH_PORT_NULL;
 
         unsafe {
-            kern_try!(sys::IOMasterPort(
-                sys::kIOMasterPortDefault,
-                &mut master_port
-            ));
+            kern_try!(IOMasterPort(kIOMasterPortDefault, &mut master_port));
         }
 
         Ok(IoMasterPort(master_port))
     }
 
     pub fn get_services(&self) -> Result<IoIterator> {
-        let service = unsafe {
-            let ret = sys::IOServiceMatching(sys::IOPM_SERVICE_NAME.as_ptr());
-            assert_ne!(ret as *const _, kCFNull);
-
-            ret
-        };
+        let service = unsafe { IOServiceMatching(IOPM_SERVICE_NAME.as_ptr()) }.ok_or(
+            Error::not_found(format!(
+                "Cannot find IOService named {}",
+                IOPM_SERVICE_NAME.to_string_lossy()
+            )),
+        );
 
         let mut iterator = IoIterator::default();
 
         unsafe {
-            kern_try!(sys::IOServiceGetMatchingServices(
+            kern_try!(IOServiceGetMatchingServices(
                 self.0,
-                service,
+                Some(CFRetained::cast_unchecked::<CFDictionary>(service?)),
                 &mut *iterator
             ));
         }
@@ -56,42 +63,42 @@ impl Drop for IoMasterPort {
 }
 
 #[derive(Debug)]
-pub struct IoObject(sys::io_object_t);
+pub struct IoObject(io_object_t);
 
 impl IoObject {
     /// Returns typed dictionary with this object properties.
     /// In our case all keys are CFStrings, so there is no need to return
     /// untyped dict here.
-    pub fn properties(&self) -> Result<CFDictionary<CFString, CFType>> {
+    pub fn properties(&self) -> Result<CFRetained<CFDictionary<CFString, CFType>>> {
         unsafe {
-            let mut props = MaybeUninit::<CFMutableDictionaryRef>::zeroed();
+            let mut props = std::ptr::null_mut();
 
-            kern_try!(sys::IORegistryEntryCreateCFProperties(
+            kern_try!(IORegistryEntryCreateCFProperties(
                 self.0,
-                props.as_mut_ptr(),
+                &mut props,
                 kCFAllocatorDefault,
                 0
             ));
+            let data = CFRetained::<CFMutableDictionary>::from_raw(NonNull::new_unchecked(props));
+            let result = CFRetained::cast_unchecked::<CFDictionary<CFString, CFType>>(data);
 
-            let props = props.assume_init();
-
-            Ok(CFMutableDictionary::wrap_under_create_rule(props).to_immutable())
+            Ok(result)
         }
     }
 }
 
 impl Drop for IoObject {
     fn drop(&mut self) {
-        let result = unsafe { sys::IOObjectRelease(self.0) };
+        let result = IOObjectRelease(self.0);
         assert_eq!(result, kern_return::KERN_SUCCESS);
     }
 }
 
 #[derive(Debug)]
-pub struct IoIterator(sys::io_iterator_t);
+pub struct IoIterator(io_iterator_t);
 
 impl Deref for IoIterator {
-    type Target = sys::io_iterator_t;
+    type Target = io_iterator_t;
 
     fn deref(&self) -> &Self::Target {
         &self.0
@@ -108,7 +115,7 @@ impl Iterator for IoIterator {
     type Item = IoObject;
 
     fn next(&mut self) -> Option<Self::Item> {
-        match unsafe { sys::IOIteratorNext(self.0) } {
+        match IOIteratorNext(self.0) {
             0 => None, // TODO: Should not there be some `NULL`?
             io_object => Some(IoObject(io_object)),
         }
@@ -117,7 +124,7 @@ impl Iterator for IoIterator {
 
 impl Drop for IoIterator {
     fn drop(&mut self) {
-        let result = unsafe { sys::IOObjectRelease(self.0) };
+        let result = IOObjectRelease(self.0);
         assert_eq!(result, kern_return::KERN_SUCCESS);
     }
 }
